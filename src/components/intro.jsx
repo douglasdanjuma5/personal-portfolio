bb76105bf9d75093e37d922258153ef4aeffc16b
douglasdanjuma5 <douglasdanjuma9@gmail.com>
2021-07-12 12:28:54 +0100

    Update intro.jsx

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -16,7 +16,7 @@ class Intro extends React.Component{
                 <div className="container">
                   <h1 className="intro-title mb-4">Hello, I am Douglas Danjuma</h1>
                   <p className="intro-subtitle"><span className="text-slider-items">
-                    </span><strong className="text-slider"><Typed strings={['Front End Developer', 'Back End Developer', 'Software Engineer','Freelancer']} typeSpeed = {80} backDelay = {1100} backSpeed = {30} loop/></strong></p>
+                    </span><strong className="text-slider"><Typed strings={['Fulstack Developer', 'Software Engineer','Freelancer']} typeSpeed = {80} backDelay = {1100} backSpeed = {30} loop/></strong></p>
                   <p className="pt-3"><a className="btn btn-primary btn js-scroll px-4" href="#about" role="button">View About Me</a></p>
                 </div>
               </div>
